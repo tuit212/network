d285d4683508d365e3726b75bf9a5a490b2ec89d
tuit212 <asadbekanvarov9808@example.com>
2023-09-19 01:16:34 +0500

    firist commet

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NotFound.scss'
+
+const NotFound = () => {
+  return (
+    <div className='notFound'>
+        Siz qidirgan sahifa afsuski topilmadi
+    </div>
+  )
+}
+
+export default NotFound
